2dc0e9584c65a2b4c5dc1cb7732eda7571885515
Ivan Harbatsenka <1995van95@gmail.com>
2022-12-22 21:01:30 +0300

    Убрал код из коментов

--- Практическое задание №3/three.py
+++ Практическое задание №3/three.py
@@ -14,29 +14,29 @@ from langdetect import detect
 # 1 2 1 2 2
 # Вывод: 2
 
-# user_input_number = int(input('Введите число для определения размера массива: '))
-# check_digit = int(input('Введите число которое нужно проверить: '))
-# print(
-#     f'И так, размер будет равен вашему числу: {user_input_number} делённому на 2. (округление будет в большую сторону)')
-#
-# digit_for_list = math.ceil(user_input_number / 2)
-# random_list = []
-#
-# while digit_for_list > 0:
-#     random_list.append(random.randint(1, user_input_number))
-#     digit_for_list -= 1
-# print('Ваш массив состоит из данных чисел: ', end=' ')
-# print(*random_list, sep=', ')
-#
-# count = 0
-# for i in random_list:
-#     print(type(i))
-#     if i == check_digit:
-#         count += 1
-#     else:
-#         continue
-#
-# print(f'Ваше число: {check_digit}, встречается в массиве: {count} раз.')
+user_input_number = int(input('Введите число для определения размера массива: '))
+check_digit = int(input('Введите число которое нужно проверить: '))
+print(
+    f'И так, размер будет равен вашему числу: {user_input_number} делённому на 2. (округление будет в большую сторону)')
+
+digit_for_list = math.ceil(user_input_number / 2)
+random_list = []
+
+while digit_for_list > 0:
+    random_list.append(random.randint(1, user_input_number))
+    digit_for_list -= 1
+print('Ваш массив состоит из данных чисел: ', end=' ')
+print(*random_list, sep=', ')
+
+count = 0
+for i in random_list:
+    print(type(i))
+    if i == check_digit:
+        count += 1
+    else:
+        continue
+
+print(f'Ваше число: {check_digit}, встречается в массиве: {count} раз.')
 
 # Задача 18:
 # # Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
@@ -50,39 +50,39 @@ from langdetect import detect
 # # Вывод: 6
 #
 #
-# user_list_digit = int(input('Введите число для определения размера массива: '))
-# check_list_digit = int(input('Введите число которое нужно проверить: '))
-# user_list = []
-# list_count = user_list_digit
-#
-# while list_count > 0:
-#     user_list.append(random.randint(0, user_list_digit))
-#     list_count -= 1
-# print(user_list)
-#
-# if check_list_digit in user_list:
-#     print(f'Число {check_list_digit} есть в списке. Но оно нам не подходит')
-#
-# digit_in_list_minus = check_list_digit - 1
-# digit_in_list_plus = check_list_digit + 1
-# while True:
-#     if digit_in_list_minus in user_list:
-#         break
-#     else:
-#         digit_in_list_minus -= 1
-#
-# while True:
-#     if digit_in_list_plus in user_list:
-#         break
-#     else:
-#         digit_in_list_plus += 1
-#
-# if check_list_digit - digit_in_list_minus == abs(check_list_digit - digit_in_list_plus):
-#     print(f'В списке два числа одинаково близких к вашему: \n1) {digit_in_list_plus}\n2) {digit_in_list_minus}')
-# elif check_list_digit - digit_in_list_minus < abs(check_list_digit - digit_in_list_plus):
-#     print(f'Ближайшее число: {digit_in_list_minus}')
-# elif check_list_digit - digit_in_list_minus > abs(check_list_digit - digit_in_list_plus):
-#     print(f'ближайшее число: {digit_in_list_plus}')
+user_list_digit = int(input('Введите число для определения размера массива: '))
+check_list_digit = int(input('Введите число которое нужно проверить: '))
+user_list = []
+list_count = user_list_digit
+
+while list_count > 0:
+    user_list.append(random.randint(0, user_list_digit))
+    list_count -= 1
+print(user_list)
+
+if check_list_digit in user_list:
+    print(f'Число {check_list_digit} есть в списке. Но оно нам не подходит')
+
+digit_in_list_minus = check_list_digit - 1
+digit_in_list_plus = check_list_digit + 1
+while True:
+    if digit_in_list_minus in user_list:
+        break
+    else:
+        digit_in_list_minus -= 1
+
+while True:
+    if digit_in_list_plus in user_list:
+        break
+    else:
+        digit_in_list_plus += 1
+
+if check_list_digit - digit_in_list_minus == abs(check_list_digit - digit_in_list_plus):
+    print(f'В списке два числа одинаково близких к вашему: \n1) {digit_in_list_plus}\n2) {digit_in_list_minus}')
+elif check_list_digit - digit_in_list_minus < abs(check_list_digit - digit_in_list_plus):
+    print(f'Ближайшее число: {digit_in_list_minus}')
+elif check_list_digit - digit_in_list_minus > abs(check_list_digit - digit_in_list_plus):
+    print(f'ближайшее число: {digit_in_list_plus}')
 
 # Задача 20:
 # В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
